5371bb992ea0b25ac6edafdec997adc457c70c0c
david de la puente enriquez <Twinkymen@hotmail.com>
2022-03-31 13:16:14 +0200

    Actualizado MySql

--- src/main/java/tecnocampus/demorocketsmovement/RocketRestController.java
+++ src/main/java/tecnocampus/demorocketsmovement/RocketRestController.java
@@ -12,9 +12,21 @@ public class RocketRestController {
     private RocketRepository rocketRepository;
     private PropellantRepository propellantRepository;
 
-    public RocketRestController(RocketRepository rocketRepository) {
+    public RocketRestController(RocketRepository rocketRepository, PropellantRepository propellantRepository) {
         this.rocketRepository = rocketRepository;
-        this.propellantRepository = null;
+        this.propellantRepository = propellantRepository;
+    }
+
+    @PutMapping("/rockets/{rocketId}")
+    public Rocket updateRepository(@PathVariable Long rocketId, @RequestBody Rocket rocket){
+        return rocketRepository.save(rocket);
+    }
+
+    @PutMapping("/rockets/{rocketId}/propellants/{propellantId}")
+    public Propellant updateMaxPower(@PathVariable Long propellantId, @RequestBody int maxPower) {
+        Propellant propellant = propellantRepository.findById(propellantId).get();
+        propellant.setMaxPower(maxPower);
+        return propellantRepository.save(propellant);
     }
 
     @GetMapping("/rockets")
@@ -43,6 +55,25 @@ public class RocketRestController {
     public List<Propellant> getPropellants(@PathVariable Long id){
         Rocket rocket = rocketRepository.getById(id);
         return rocket.getPropellants();
+    }
+
+    @DeleteMapping("/rockets")
+    public List<Rocket> deleteRockets() {
+        rocketRepository.deleteAll();
+        return rocketRepository.findAll();
+    }
+
+    @DeleteMapping("/rockets/{rocketId}")
+    public void deleteRocket(@PathVariable Long rocketId) { rocketRepository.deleteById(rocketId);}
+
+    @DeleteMapping("/Propellant/propellants")
+    public List<Propellant> deletePropellants(@PathVariable Long rocketId) {
+        return null;
+    }
+
+    @DeleteMapping("/propellants/{propellantId}")
+    public void deletePropellant(@PathVariable Long propellantId){
+        propellantRepository.deleteById(propellantId);
     }
 
     @PostMapping("/rockets/{id}/movement")
